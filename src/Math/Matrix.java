98eb065392142d8fa73f683c0e2d6fd6ae2018a6
akherati56 <akherati5660@gmail.com>
2024-11-05 20:17:15 +0330

    final commit

---
+++ src/Math/Matrix.java
@@ -0,0 +1,25 @@
+package Math;
+
+public class Matrix {
+
+    private int rows;
+    private int cols;
+
+    public Matrix(int size) {
+        rows = size;
+        cols = size;
+    }
+
+    public int[][] getMatrix() {
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        return matrix;
+    }
+
+
+
+}
---
+++ src/University/Grade.java
@@ -0,0 +1,18 @@
+package University;
+
+public class Grade {
+    public String convert_grades(int grade){
+        if (grade > 100 | grade < 0) {
+            throw new IllegalArgumentException("Invalid grade");
+        }
+        grade = grade / 10;
+        return switch (grade) {
+            case 9 -> "A";
+            case 8 -> "B";
+            case 7 -> "C";
+            case 6 -> "D";
+            case 5 -> "E";
+            default -> "F";
+        };
+    }
+}
